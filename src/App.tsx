b8af248010c3a6c208f6702e6b7fd32ca9a0406f
alex <amoore16@gmail.com>
2024-06-23 19:38:59 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState<any>([]);
+
+  useEffect(() => {
+    const api = async () => {
+      const fetchdata = await fetch("http://localhost:3000/hello", {
+        method: "GET",
+      });
+      const jsonData = await fetchdata.json();
+      console.log(jsonData);
+      setData(jsonData.message);
+    };
+
+    api();
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{data ? data : "Loading..."}</div>
     </div>
   );
 }
